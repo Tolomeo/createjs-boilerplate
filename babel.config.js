d97f861584a7d8003ed0c00ebb4bc06e2cf2d719
Diego Frattini <dgfrattini@gmail.com>
2021-05-08 12:30:07 +0100

    chore: Babel config removal

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [["@babel/preset-env", { targets: "> 0.25%, not dead" }]],
-  env: {
-    test: {
-      plugins: ["@babel/plugin-transform-modules-commonjs"],
-    },
-  },
-};
