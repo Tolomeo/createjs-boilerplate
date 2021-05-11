9941240d4736fb6e82aa1985e5cbc8a72d23211e
Diego Frattini <dgfrattini@gmail.com>
2021-05-11 22:59:49 +0100

    chore: Setup script tweaks

--- scripts/setup.js
+++ scripts/setup.js
@@ -7,26 +7,28 @@ const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
 
+const rootDirectory = fs.realpathSync(process.cwd());
+const scriptsDirectory = path.resolve(rootDirectory, "/scripts");
 const README_CONTENT = `
-# This is README of you project
+# Project README
 
-Some informations
+It is nice to document properly your project here, so other developers won't struggle getting on board.
+Even your future self will benefit from this.
+
+Have a look [here](https://github.com/othneildrew/Best-README-Template) for a nice starting template.
 `;
 
-const callback = (string) => (err) => {
-  if (err) {
-    throw err;
-  }
+// 1. removing .git dir
+shell.rm("-rf", path.join(rootDirectory, ".vscode"));
+// 2. removing setup script 
+fs.unlinkSync(path.join(scriptsDirectory, "setup.js"));
+// 3. overriding README
+fs.writeFileSync(path.join(rootDirectory, "README.md"), README_CONTENT);
+// 4. initialising a new git project
+// exec('git init && git add . && git commit -m "Initial commit"', (err) => {
+//   if (err) {
+//     throw err;
+//   }
 
-  console.log(string);
-};
-
-shell.rm("-rf", ".git");
-fs.unlink(path.join(__dirname, "setup.js"), () => {});
-fs.unlink(path.join(__dirname, "README.md"), () => {});
-fs.writeFile("README.md", README_CONTENT, () => {});
-exec(
-  // eslint-disable-next-line
-  'git init && git add . && git commit -m "Initial commit"',
-  callback("Repository initialized"),
-);
+//   console.log("Done. Enjoy!");
+// });
