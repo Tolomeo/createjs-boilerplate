4a098867dbbc54f7de962848feb2287be00e7213
Diego Frattini <dgfrattini@gmail.com>
2021-05-12 11:18:15 +0100

    chore: Setup script tweaks

--- scripts/setup.js
+++ scripts/setup.js
@@ -3,13 +3,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 const shell = require("shelljs");
-const chalk = require('chalk');
+const chalk = require("chalk");
 // const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
 
 const rootDirectory = fs.realpathSync(process.cwd());
-const gitDirectory = path.join(rootDirectory, ".vscode")
+const gitDirectory = path.join(rootDirectory, ".git");
 const setupScript = path.resolve(rootDirectory, path.join("scripts", "setup.js"));
 const readmeFile = path.resolve(rootDirectory, "README.md");
 const readmeContent = `
@@ -22,21 +22,18 @@ Have a look [here](https://github.com/othneildrew/Best-README-Template) for a ni
 `;
 
 // 1. removing .git dir
-console.info(chalk.blueBright(`Removing git directory at ${gitDirectory}\n`));
+console.info(chalk.blueBright(`Removing git directory at ${gitDirectory}`));
 shell.rm("-rf", gitDirectory);
-// 2. removing setup script 
-console.info(chalk.blueBright(`Removing setup script file at ${setupScript}\n`));
+// 2. removing setup script
+console.info(chalk.blueBright(`Removing setup script file at ${setupScript}`));
 shell.rm(setupScript);
 // 3. overriding README
-console.info(chalk.blueBright(`Writing readme file at ${readmeFile}\n`));
+console.info(chalk.blueBright(`Writing readme file at ${readmeFile}`));
 shell.ShellString(readmeContent).to(readmeFile);
-
 // 4. initialising a new git project
-// exec('git init && git add . && git commit -m "Initial commit"', (err) => {
-//   if (err) {
-//     throw err;
-//   }
-
-//   console.log("Done. Enjoy!");
-// });
-console.info(chalk.green(`"Done. Enjoy!"\n`));
+console.info(chalk.blueBright(`Initialising new git project`));
+shell.exec(`git init`);
+shell.exec(`git add .`);
+shell.exec(`git commit -m "Initial commit"`);
+// Done
+console.info(chalk.green(`Done. Enjoy!`));
