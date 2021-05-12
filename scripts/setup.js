f56629d608eb38e02d6485cff70e88a01a25a3f0
Diego Frattini <dgfrattini@gmail.com>
2021-05-12 11:23:16 +0100

    Setup script tweaks(#3)
    * chore: Setup script tweaks
    
    * chore: Setup script tweaks
    
    * chore: Setup script tweaks
    
    * chore: Setup script tweaks
    
    * chore: Setup script tweaks
    
    * fix: Leftover comment

--- scripts/setup.js
+++ scripts/setup.js
@@ -3,30 +3,36 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 const shell = require("shelljs");
-const { exec } = require("child_process");
+const chalk = require("chalk");
 const path = require("path");
 const fs = require("fs");
 
-const README_CONTENT = `
-# This is README of you project
+const rootDirectory = fs.realpathSync(process.cwd());
+const gitDirectory = path.join(rootDirectory, ".git");
+const setupScript = path.resolve(rootDirectory, path.join("scripts", "setup.js"));
+const readmeFile = path.resolve(rootDirectory, "README.md");
+const readmeContent = `
+# Project README
 
-Some informations
+It is nice to document properly your project here, so other developers won't struggle getting on board.
+Even your future self will benefit from this.
+
+Have a look [here](https://github.com/othneildrew/Best-README-Template) for a nice starting template.
 `;
 
-const callback = (string) => (err) => {
-  if (err) {
-    throw err;
-  }
-
-  console.log(string);
-};
-
-shell.rm("-rf", ".git");
-fs.unlink(path.join(__dirname, "setup.js"), () => {});
-fs.unlink(path.join(__dirname, "README.md"), () => {});
-fs.writeFile("README.md", README_CONTENT, () => {});
-exec(
-  // eslint-disable-next-line
-  'git init && git add . && git commit -m "Initial commit"',
-  callback("Repository initialized"),
-);
+// 1. removing .git dir
+console.info(chalk.blueBright(`Removing git directory at ${gitDirectory}`));
+shell.rm("-rf", gitDirectory);
+// 2. removing setup script
+console.info(chalk.blueBright(`Removing setup script file at ${setupScript}`));
+shell.rm(setupScript);
+// 3. overriding README
+console.info(chalk.blueBright(`Writing readme file at ${readmeFile}`));
+shell.ShellString(readmeContent).to(readmeFile);
+// 4. initialising a new git project
+console.info(chalk.blueBright(`Initialising new git project`));
+shell.exec(`git init`);
+shell.exec(`git add .`);
+shell.exec(`git commit -m "Initial commit"`);
+// Done
+console.info(chalk.green(`Done. Enjoy!`));
