12ebac878f6c2de76e823374c40ec7e654412019
Diego Frattini <dgfrattini@gmail.com>
2021-05-19 00:33:12 +0100

    chore: Config dir to .webpack
    It contains webpack configs only

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const development = require("./config/webpack.development");
-const production = require("./config/webpack.production");
+const development = require("./.webpack/webpack.development");
+const production = require("./.webpack/webpack.production");
 
 const environment = {
   development,
